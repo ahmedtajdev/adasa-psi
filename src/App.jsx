00f1a5a1fb2827ab14495b97a16cf7f30a527c2c
ahmedtajdev <ahmedtaj.dev@gmail.com>
2026-02-02 13:52:26 +0200

    createHashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import {
   createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "../node_modules/react-router-dom";
 import Layout from "./Components/Layout/Layout";
@@ -12,7 +13,7 @@ import data from "./data.json";
 import BlogDetails from "./Components/BlogDetails/BlogDetails";
 import ErrorPage from "./Components/ErrorPage/ErrorPage";
 
-let router = createBrowserRouter([
+let router = createHashRouter([
   {
     path: "adasa-psi",
     element: <Layout />,
